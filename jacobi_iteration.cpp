f933c4f8d7a0bcee20440c2ae12dae03fe83db15
Pierre-Emmanuel Goffi <chapsag@gmail.com>
2021-03-15 19:20:25 -0400

    MPI Hello world, working

--- jacobi_iteration.cpp
+++ jacobi_iteration.cpp
@@ -1,12 +1,25 @@
-#include <mpi.h>
+#include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <mpi.h>
 
-int main(int argc, char** argv) {
-    MPI_Init(NULL, NULL);
-    int rank;
-    int world;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world);
-    printf("Hello: rank %d, world: %d\n",rank, world);
-    MPI_Finalize();
+int main(int argc, char * argv[])
+{
+  MPI_Init(&argc, &argv);
+
+  int rank;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) {
+    char hello[] = "Hello, World";
+    MPI_Send(hello, 12,  MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+  }
+  else if (rank == 1) {
+    char hello[256];
+    MPI_Recv(hello, 12, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+    printf("Rank 1 received string `%s' from Rank 0.\n", hello);
+  }
+
+  MPI_Finalize();
+  return 0;
 }
